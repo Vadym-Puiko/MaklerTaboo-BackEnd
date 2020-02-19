8cbd08fbfec3556be6bed04c3d2d32d6b864d41e
Vadym-pk <57877094+Vadym-pk@users.noreply.github.com>
2020-02-19 13:33:15 +0200

    created branches and main package

---
+++ src/test/java/makler/lv480/java/ApplicationTests.java
@@ -0,0 +1,13 @@
+package makler.lv480.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
