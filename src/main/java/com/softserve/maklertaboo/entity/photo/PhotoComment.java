670cf320c69159e73bfe242192188d59b91f78fd
Latyk Borys <boris21352@gmail.com>
2020-02-20 23:06:59 +0200

    Created entity passport

--- src/main/java/com/softserve/maklertaboo/entity/photo/PhotoComment.java
+++ src/main/java/com/softserve/maklertaboo/entity/photo/PhotoComment.java
@@ -2,8 +2,9 @@ package com.softserve.maklertaboo.entity.photo;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-
+@Entity
 @Data
 public class PhotoComment extends PhotoAbstract {
 
