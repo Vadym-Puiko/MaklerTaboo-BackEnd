cfdc87f3510e31850811b702d66c7e2fb31c8976
Mykola3303 <myk.borovets@gmail.com>
2020-02-20 19:27:04 +0200

    add Flat entity

---
+++ src/main/java/com/softserve/maklertaboo/domain/entity/Flat.java
@@ -0,0 +1,41 @@
+package com.softserve.maklertaboo.domain.entity;
+
+import lombok.Data;
+import lombok.Generated;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Flat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    private Address address;
+
+    @OneToMany
+    private List<PhotoFlat> photoFlatList;
+
+    @ManyToOne
+    private User owner;
+
+    @ManyToMany
+    List<Tags> tagsList;
+
+    @OneToMany
+    List<Order> order;
+
+    @OneToMany
+    List<CommentFlat> commentFlatList;
+
+    @Column (nullable = false)
+    private String description;
+    @Column (nullable = false)
+    private String title;
+    private boolean isVisible;
+    @Column (nullable = false)
+    private double monthPrice;
+}
