438324c9777bc8f7991ec612eacb7f891ae6c1b6
MikeOstapiuk <ostapiuk.mykhailo@gmail.com>
2020-02-20 18:41:44 +0200

    Added Order, OrderStatus, Tag entities.

---
+++ src/main/java/com/softserve/maklertaboo/entity/Order.java
@@ -0,0 +1,31 @@
+package com.softserve.maklertaboo.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private Flat flat;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private OrderStatus orderStatus;
+
+    private double price;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    private int duration;
+}
