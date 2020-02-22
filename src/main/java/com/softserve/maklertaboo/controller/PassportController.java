ad7639b1edd4b61779bab07d7a3f8bf7aff99425
Latyk Borys <boris21352@gmail.com>
2020-02-22 14:26:49 +0200

    Created controller for passport entity and repository

---
+++ src/main/java/com/softserve/maklertaboo/controller/PassportController.java
@@ -0,0 +1,25 @@
+package com.softserve.maklertaboo.controller;
+
+import com.softserve.maklertaboo.entity.Passport;
+import com.softserve.maklertaboo.repository.passport.PassportRepository;
+import com.sun.istack.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class PassportController {
+   Logger logger=Logger.getLogger(PassportController.class);
+    @Autowired
+    PassportRepository passportRepository;
+
+    @GetMapping("/passport")
+    public void  getAllPassport(){
+      List<Passport> passportList= passportRepository.findAll();
+        logger.info(String.valueOf(passportList));
+    }
+}
