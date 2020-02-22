a70d09d199debfd0bfb9213cc327e07e4eed49b5
maksym-huliaka <maxim.lviv1@gmail.com>
2020-02-22 16:26:54 +0200

    Json ,cascade

--- src/main/java/com/softserve/maklertaboo/entity/photo/PhotoAbstract.java
+++ src/main/java/com/softserve/maklertaboo/entity/photo/PhotoAbstract.java
@@ -1,23 +1,38 @@
 package com.softserve.maklertaboo.entity.photo;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.softserve.maklertaboo.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public abstract class PhotoAbstract {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @JsonIdentityInfo(
+            generator = ObjectIdGenerators.PropertyGenerator.class,
+            property = "id")
+    @ManyToOne(cascade = {
+            CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.LAZY)
     private User userAuthor;
 
-    private String urlPhoto;
-    private LocalDateTime dateOfCreation;
+    @Column(name = "date")
+    private LocalDateTime date;
+
+    @Column(name = "url")
+    private String url;
 
 }
