120c57aec89b83ff9042c770eda60e733d0d72e7
Latyk Borys <boris21352@gmail.com>
2020-02-22 15:03:05 +0200

    Fixed entity, deleted properties from gitignore

--- src/main/java/com/softserve/maklertaboo/entity/Passport.java
+++ src/main/java/com/softserve/maklertaboo/entity/Passport.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.util.List;
-import java.util.Objects;
+
 
 @Data
 @Entity
@@ -39,53 +39,4 @@ public class Passport {
     @OneToMany
     private List<PassportPhoto> passportPhotoList;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Passport passport = (Passport) o;
-        return gender == passport.gender &&
-                passportType == passport.passportType &&
-                identificationNumber == passport.identificationNumber &&
-                Objects.equals(id, passport.id) &&
-                Objects.equals(firstName, passport.firstName) &&
-                Objects.equals(lastName, passport.lastName) &&
-                Objects.equals(middleName, passport.middleName) &&
-                Objects.equals(birthDate, passport.birthDate) &&
-                Objects.equals(birthPlace, passport.birthPlace) &&
-                Objects.equals(nationality, passport.nationality) &&
-                Objects.equals(authority, passport.authority) &&
-                Objects.equals(dateOfIssue, passport.dateOfIssue) &&
-                Objects.equals(expirationDate, passport.expirationDate) &&
-                Objects.equals(passportNumber, passport.passportNumber) &&
-                Objects.equals(user, passport.user) &&
-                Objects.equals(passportPhotoList, passport.passportPhotoList);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, firstName, lastName, middleName, gender, birthDate, birthPlace, passportType, nationality, authority, dateOfIssue, expirationDate, passportNumber, identificationNumber, user, passportPhotoList);
-    }
-
-    @Override
-    public String toString() {
-        return "Passport{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", middleName='" + middleName + '\'' +
-                ", gender=" + gender +
-                ", birthDate='" + birthDate + '\'' +
-                ", birthPlace='" + birthPlace + '\'' +
-                ", passportType=" + passportType +
-                ", nationality='" + nationality + '\'' +
-                ", authority='" + authority + '\'' +
-                ", dateOfIssue='" + dateOfIssue + '\'' +
-                ", expirationDate='" + expirationDate + '\'' +
-                ", passportNumber='" + passportNumber + '\'' +
-                ", identificationNumber=" + identificationNumber +
-                ", user=" + user +
-                ", passportPhotoList=" + passportPhotoList +
-                '}';
-    }
 }
