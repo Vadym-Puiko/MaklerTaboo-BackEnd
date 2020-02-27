af6b756d2b0ae2db34b6c5159f72c55e4bfdfb00
Vadym-pk <57877094+Vadym-pk@users.noreply.github.com>
2020-02-27 12:06:25 +0200

    added dependency and fix swagger

---
+++ src/main/java/com/softserve/maklertaboo/mapping/UserToDto.java
@@ -0,0 +1,21 @@
+package com.softserve.maklertaboo.mapping;
+
+import com.softserve.maklertaboo.dto.user.UserDto;
+import com.softserve.maklertaboo.entity.User;
+
+public class UserToDto implements MapperToDto<User, UserDto> {
+
+    private UserDto userDto = new UserDto();
+
+    @Override
+    public UserDto convertToDto(User user) {
+        if (user == null) {
+            user = new User();
+        }
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+        user.setPhoneNumber(userDto.getPhoneNumber());
+        user.setPhotoUrl(userDto.getPhotoUrl());
+        return userDto;
+    }
+}
--- src/main/java/com/softserve/maklertaboo/service/UserService.java
+++ src/main/java/com/softserve/maklertaboo/service/UserService.java
@@ -2,7 +2,9 @@ package com.softserve.maklertaboo.service;
 
 import com.softserve.maklertaboo.dto.user.UserDto;
 import com.softserve.maklertaboo.entity.User;
-import com.softserve.maklertaboo.repository.photo.UserRepository;
+import com.softserve.maklertaboo.mapping.UserToDto;
+import com.softserve.maklertaboo.mapping.UserToEntity;
+import com.softserve.maklertaboo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +17,8 @@ public class UserService {
     private UserRepository userRepository;
 
     public void save(UserDto userDto) {
-        User user = userToUserRequest(userDto, null);
+        UserToEntity userToEntity = new UserToEntity();
+        User user = userToEntity.convertToEntity(userDto);
         userRepository.save(user);
     }
 
@@ -29,24 +32,15 @@ public class UserService {
     public User findOne(Long id) {
         return userRepository.findById(id).orElseThrow(IllegalArgumentException::new);
     }
-
-    public void update(Long id, UserDto userDto) {
-        User user = userToUserRequest(userDto, findOne(id));
-        userRepository.save(user);
-    }
+//
+//    public void update(Long id, UserDto userDto) {
+//        User user = userToUserRequest(userDto, findOne(id));
+//        userRepository.save(user);
+//    }
 
     public void delete(Long id) {
         userRepository.deleteById(id);
     }
 
-    private User userToUserRequest(UserDto userDto, User user) {
-        if (user == null) {
-            user = new User();
-        }
-        user.setEmail(userDto.getEmail());
-        user.setPassword(userDto.getPassword());
-        user.setPhoneNumber(userDto.getPhoneNumber());
-        user.setPhotoUrl(userDto.getPhotoUrl());
-        return user;
-    }
+
 }
