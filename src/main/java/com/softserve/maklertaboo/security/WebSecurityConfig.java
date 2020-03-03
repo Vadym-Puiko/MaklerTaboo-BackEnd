3d2955b30008908151e253679c3dfd44ac1dff27
MikeOstapiuk <ostapiuk.mykhailo@gmail.com>
2020-03-03 10:53:45 +0200

    Change webConfig

--- src/main/java/com/softserve/maklertaboo/security/WebSecurityConfig.java
+++ src/main/java/com/softserve/maklertaboo/security/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/users").permitAll()
+                .antMatchers("/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
