3f2ea7b83c6e4a31cba384c5f1a798a23b59cb79
AndriyPyzh <andriy.pyzh2412@gmail.com>
2020-03-03 15:48:52 +0200

    added AdminController

---
+++ src/main/java/com/softserve/maklertaboo/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.softserve.maklertaboo.controller;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Data
+@CrossOrigin
+@RequestMapping("/admin")
+public class AdminController {
+
+}
