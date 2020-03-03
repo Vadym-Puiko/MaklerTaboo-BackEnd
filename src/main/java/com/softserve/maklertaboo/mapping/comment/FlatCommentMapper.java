fe43200d56aca988bec151c9cc2c502a3693dd82
IsachenkoDima <idimon777@gmail.com>
2020-03-03 14:23:05 +0200

    created flat and user comment dto

--- src/main/java/com/softserve/maklertaboo/mapping/comment/FlatCommentMapper.java
+++ src/main/java/com/softserve/maklertaboo/mapping/comment/FlatCommentMapper.java
@@ -3,8 +3,13 @@ package com.softserve.maklertaboo.mapping.comment;
 import com.softserve.maklertaboo.dto.comment.FlatCommentDto;
 import com.softserve.maklertaboo.entity.Flat;
 import com.softserve.maklertaboo.entity.comment.FlatComment;
+import com.softserve.maklertaboo.entity.photo.CommentPhoto;
+import com.softserve.maklertaboo.entity.photo.FlatPhoto;
 import com.softserve.maklertaboo.mapping.MapperToDto;
 import com.softserve.maklertaboo.mapping.MapperToEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FlatCommentMapper implements MapperToDto<FlatComment, FlatCommentDto>, MapperToEntity<FlatCommentDto, FlatComment> {
     @Override
@@ -25,7 +30,10 @@ public class FlatCommentMapper implements MapperToDto<FlatComment, FlatCommentDt
         Flat flat=new Flat();
         flat.setId(dto.getPostId());
         flatComment.setFlat(flat);
-        flatComment.setCommentPhotos();
+
+
+
+       // flatComment.setCommentPhotos();
 
         return null;
     }
