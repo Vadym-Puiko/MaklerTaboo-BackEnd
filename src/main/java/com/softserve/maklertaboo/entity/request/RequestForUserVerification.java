1cfee2f65a6d634b3c3da0a44bdb71106ff744eb
AndriyPyzh <andriy.pyzh2412@gmail.com>
2020-03-03 16:43:40 +0200

    minor changes

--- src/main/java/com/softserve/maklertaboo/entity/request/RequestForUserVerification.java
+++ src/main/java/com/softserve/maklertaboo/entity/request/RequestForUserVerification.java
@@ -3,12 +3,16 @@ package com.softserve.maklertaboo.entity.request;
 import com.softserve.maklertaboo.entity.user.User;
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Data
 @Entity
 @Table(name = "requests_user_verification")
 public class RequestForUserVerification extends RequestForVerification {
+
     @OneToOne(cascade = CascadeType.ALL)
     private User user;
 }
