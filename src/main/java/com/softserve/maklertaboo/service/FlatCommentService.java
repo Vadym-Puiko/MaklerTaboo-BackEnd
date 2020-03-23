92b91d9e5576133d07a5585f07930a04dc2023cb
IsachenkoDima <idimon777@gmail.com>
2020-03-23 10:19:09 +0200

    added comment about comment

--- src/main/java/com/softserve/maklertaboo/service/FlatCommentService.java
+++ src/main/java/com/softserve/maklertaboo/service/FlatCommentService.java
@@ -1,15 +1,20 @@
 package com.softserve.maklertaboo.service;
 
+import com.softserve.maklertaboo.controller.UserController;
 import com.softserve.maklertaboo.dto.comment.FlatCommentDto;
 import com.softserve.maklertaboo.entity.comment.FlatComment;
 import com.softserve.maklertaboo.entity.comment.UserComment;
 import com.softserve.maklertaboo.entity.flat.Flat;
+import com.softserve.maklertaboo.entity.user.User;
 import com.softserve.maklertaboo.mapping.comment.FlatCommentMapper;
 import com.softserve.maklertaboo.repository.FlatRepository;
 import com.softserve.maklertaboo.repository.comment.FlatCommentRepository;
+import com.softserve.maklertaboo.repository.user.UserRepository;
+import com.softserve.maklertaboo.security.jwt.JWTTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,17 +24,32 @@ public class FlatCommentService {
     private final FlatCommentRepository flatCommentRepository;
     private final FlatCommentMapper flatCommentMapper;
     private final FlatRepository flatRepository;
+    private final UserRepository userRepository;
+    private final JWTTokenProvider jwtTokenProvider;
+    private final HttpServletRequest httpServletRequest;
 
     @Autowired
-    FlatCommentService(FlatCommentRepository flatCommentRepository,FlatCommentMapper flatCommentMapper,FlatRepository flatRepository){
+    FlatCommentService(FlatCommentRepository flatCommentRepository,
+                       FlatCommentMapper flatCommentMapper,
+                       FlatRepository flatRepository,
+                       UserRepository userRepository,
+                       JWTTokenProvider jwtTokenProvider,
+                       HttpServletRequest httpServletRequest){
         this.flatCommentRepository=flatCommentRepository;
         this.flatCommentMapper=flatCommentMapper;
         this.flatRepository=flatRepository;
+        this.userRepository=userRepository;
+        this.jwtTokenProvider=jwtTokenProvider;
+        this.httpServletRequest=httpServletRequest;
     }
 
 
     public void saveFlatComment(FlatCommentDto flatCommentDto){
         FlatComment flatComment=flatCommentMapper.convertToEntity(flatCommentDto);
+        String accessToken = httpServletRequest.getHeader("Authorization");
+        String email = jwtTokenProvider.getEmailFromJWT(accessToken);
+        User user = userRepository.findUserByEmail(email);
+        flatComment.setUserAuthor(user);
         flatCommentRepository.save(flatComment);
     }
     public void saveCommentAboutComment(FlatCommentDto flatCommentDto){
