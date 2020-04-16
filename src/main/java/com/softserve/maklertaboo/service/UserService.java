01a80423eb051c51b9c827643ecdf3767e624dac
Vadym Puiko <vadympk@gmail.com>
2020-04-16 12:18:09 +0300

    fixed pom.xml

--- src/main/java/com/softserve/maklertaboo/service/UserService.java
+++ src/main/java/com/softserve/maklertaboo/service/UserService.java
@@ -72,10 +72,9 @@ public class UserService {
         boolean existsUserByPhone = userRepository.existsUserByPhoneNumber(userDto.getPhoneNumber());
         if (existsUserByEmail || existsUserByUsername || existsUserByPhone) {
             throw new UserAlreadyExistsException(ErrorMessage.USER_ALREADY_EXISTS + userDto.getEmail());
-        } else {
-            User user = userMapper.convertToEntity(userDto);
-            userRepository.save(user);
         }
+        User user = userMapper.convertToEntity(userDto);
+        userRepository.save(user);
     }
 
     public Authentication getAuthentication(LoginDto loginDto) {
