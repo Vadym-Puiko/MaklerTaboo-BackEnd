d678099769e3ffb4a2caa4951dfbf38ec8871e37
IsachenkoDima <idimon777@gmail.com>
2020-04-25 19:05:59 +0300

    added test classes

--- src/test/java/com/softserve/maklertaboo/service/ComplaintServiceTest.java
+++ src/test/java/com/softserve/maklertaboo/service/ComplaintServiceTest.java
@@ -1,9 +1,19 @@
 package com.softserve.maklertaboo.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+@PrepareForTest(ComplaintService.class)
 class ComplaintServiceTest {
 
     @Test
